ce9ec9891a22ae7fa21a941881b512ae27118ddb
kanayabuno <kana0111@g.ucla.edu>
2017-11-28 20:36:14 -0800

    Second commit(needs sounds input)

---
+++ LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.kanayabuno.chromecast;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
